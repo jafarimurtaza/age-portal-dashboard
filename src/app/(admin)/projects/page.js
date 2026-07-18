276d1c8ea1c3f518549674036ba0cc3240443164
Laylawakily26 <laylawakily@gmail.com>
2026-07-18 10:29:19 +0430

    Added text of project deatils

--- src/app/(admin)/projects/page.js
+++ src/app/(admin)/projects/page.js
@@ -1,5 +1,8 @@
 import React from "react";
 
 export default function ProjectsPage() {
-  return <div>Projects</div>;
+  return <div>
+    <h1>Project</h1>
+    <h2>Project details</h2>
+  </div>;
 }
